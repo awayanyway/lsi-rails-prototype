3a5156c89685a78fccacbd6a03122a9c3b04715e
Cominch <dominic@luetjohann.com>
2014-05-22 23:29:01 +0200

    new beaglebone installation and registration sequence

--- app/assets/javascripts/beaglebone-installation.js
+++ app/assets/javascripts/beaglebone-installation.js
@@ -1,4 +1,546 @@
-function initialize(){
+
+function bbSetServer() {
+
+  var bs = require('bonescript');
+  file = '/var/lib/cloud9/server.txt';
+  bs.writeTextFile(file, document.location.host);
+
+}
+
+function bbGetServer(callback) {
+
+  var bs = require('bonescript');
+  file = '/var/lib/cloud9/server.txt';
+  bs.readTextFile('/var/lib/cloud9/server.txt', callback);
+
+}
+
+
+
+
+function bbFileExists(filename, callback_yes, callback_no) {
+
+  var b = require('bonescript');
+  b.readTextFile('/var/lib/cloud9/autorun/forever.js', function(x) {
+
+    if ((x.data != null) && (x.data.length != 0)) {
+      callback_yes();
+    } else {
+      callback_no();
+    }
+
+  });
+
+}
+
+function bbRemoveFile(filename, callback_success) {
+
+  var b = require('bonescript');
+  b.writeTextFile(filename, "", callback_success);
+
+}
+
+
+function ScriptRemoveStatus(callback) {
+
+  bbRemoveFile('/var/lib/cloud9/autorun/writestatus.js', function() {
+
+    bbRemoveFile('/var/lib/cloud9/status.txt', callback);
+
+  });
+
+}
+
+function ScriptRetrieveStatus(callback) {
+
+  console.log ("initating the status query...");
+
+  var b = require('bonescript');
+
+  b.writeTextFile('/var/lib/cloud9/autorun/writestatus.js', 'var b = require("bonescript"); '
+
+    +'var ospackages = ["python-compiler", "python-misc", "python-multiprocessing"];'
+
+    +'var npmpackages = ["dial-a-device-node", "python-misc", "python-multiprocessing"];'
+
+    +'var status = { ospackageinstallationstatus: "unknown", ospackagestatus: "unknown", ospackages: [], npmpackageinstallationstatus: "unknown", npmpackagestatus: "unknown", '
+    
+    +'dadinstallationstatus: "unknown", dadserver: "unknown", serverconnection: "unknown", internetconnection: "unknown" }; '
+
+
+    +'function bbFileExists(filename, callback_yes, callback_no) {'
+    +  'b.readTextFile(filename, function(x) {'
+    +    'if ((x.data != null) && (x.data.length != 0) && (x.data != "")) {'
+    +      'callback_yes();'
+    +    '} else {'
+    +      'callback_no();'
+    +    '}'
+    +  '});'
+    +'}; '
+
+    +'function checkOSPackageStatus(callback) {'
+    + 'var exec = require ("child_process").exec;'
+
+    + 'function opkgify(p) { return "opkg status "+p };'
+
+    +' var execstring = ospackages.map(opkgify).join(" && ");'
+
+      + 'exec (execstring, '
+
+      +'function(error, stdout, stderr) { var linearray = stdout.split("\\n"); for (var i = 0; i < linearray.length; i++) { if (linearray[i].split(":")[0] == "Package") { status.ospackages.push(linearray[i].split(":")[1].trim()) } }; if (status.ospackages.length == 3) { status.ospackagestatus = "installed"; } else {status.ospackagestatus = "not installed";}; callback() } ); '
+
+    + '}; '
+    
+    +'function checkNPMPackageStatus(callback) {'
+    + 'var exec = require ("child_process").exec;'
+    + 'exec ("npm status", '
+
+      +'function(error, stdout, stderr) { status.npmpackagestatus = "not installed"; callback() } ); '
+
+    + '}; '
+    
+    +'function checkDADServer(callback) {'
+    
+    +  'b.readTextFile("/var/lib/cloud9/server.txt", function(x) {'
+    +    'if ((x.data != null) && (x.data.length != 0)) {'
+    +      'status.server = x.data; callback(); '
+    +    '} else {'
+    +      'status.server = x.data; callback(); '
+    +    '}'
+    +  '});'
+    
+    +'}'
+    
+    
+    +'function checkDADStatus(callback) {'
+    
+    +  'bbFileExists("/var/lib/cloud9/autorun/dial-a-device-node.js", function() { status.dadinstallationstatus = "installed"; checkDADServer(callback) }, function() { status.dadinstallationstatus = "not installed"; checkDADServer(callback) });'
+        
+    + '}; '
+
+    +'function checkInternetconnection(callback) {'
+    + 'var exec = require ("child_process").exec;'
+    + 'exec ("ping www.google.com -c 4", '
+
+      +'function(error, stdout, stderr) { status.internetconnection = "connected"; callback() } ); '
+
+    + '}; '
+
+    +'function checkServerconnection(callback) {'
+    + 'var exec = require ("child_process").exec;'
+    + 'exec ("ping www.google.com -c 4", '
+
+      +'function(error, stdout, stderr) { status.serverconnection = "connected"; callback() } ); '
+
+    + '}; '
+
+    +'function lastStep() {'
+
+    + 'b.writeTextFile("/var/lib/cloud9/status.txt", JSON.stringify(status));'
+
+    + 'b.writeTextFile("/var/lib/cloud9/autorun/writestatus.js", "");'
+
+    +'}; '
+
+
+    +'bbFileExists("/var/lib/cloud9/autorun/installospackages.js", function() { '
+    
+    +   'status.ospackageinstallationstatus = "installing"; checkOSPackageStatus(function() {bbFileExists("/var/lib/cloud9/autorun/installnpmpackages.js", function() {'
+    
+    +       'status.npmpackageinstallationstatus = "installing"; checkNPMPackageStatus(function() {checkDADStatus(function(){checkInternetconnection(function(){checkServerconnection(lastStep)})})}); '
+
+    +   '}, function() {'
+    
+    +       'status.npmpackageinstallationstatus = "not installing"; checkNPMPackageStatus(function() {checkDADStatus(function(){checkInternetconnection(function(){checkServerconnection(lastStep)})})}); '
+    
+    +   '})});'
+    
+    +'}, function() { '
+    
+    +   'status.ospackageinstallationstatus = "not installing"; checkOSPackageStatus(function() {bbFileExists("/var/lib/cloud9/autorun/installnpmpackages.js", function() {'
+    
+    +       'status.npmpackageinstallationstatus = "installing";  checkNPMPackageStatus(function() {checkDADStatus(function(){checkInternetconnection(function(){checkServerconnection(lastStep)})})}); '
+
+    +   '}, function() {'
+    
+    +       'status.npmpackageinstallationstatus = "not installing"; checkNPMPackageStatus(function() {checkDADStatus(function(){checkInternetconnection(function(){checkServerconnection(lastStep)})})}); '
+    
+    +   '})});'
+    
+    +'});'
+
+
+    , callback);
+
+}
+
+function ScriptReadStatus(callback_yes, callback_no) {
+
+  console.log ("start read status");
+
+  var b = require('bonescript');
+
+  b.readTextFile('/var/lib/cloud9/status.txt', function (x) {
+
+    if ((x.data != null) && (x.data.length != 0)) {
+
+      callback_yes(JSON.parse(x.data));
+
+
+    } else {
+
+      callback_no();
+    }
+
+  });
+
+}
+
+var counter = 1;
+
+var waitseconds = 40;
+
+var global_status;
+
+var checking = false;
+
+var otheraction = false;
+
+
+function cancel() {
+
+  counter = 999;
+
+}
+
+
+function checkStatus() {
+
+  if (checking == false) {
+
+    checking = true;
+
+    counter = 1;
+
+
+    if (otheraction == false) {
+
+      setUIStatusCheckOngoing();
+
+    }
+
+    console.log ("checking status...");
+
+      ScriptRemoveStatus(function() {
+
+
+        ScriptRetrieveStatus(function() {
+
+          is = setTimeout (function cs() {
+
+            checkAgainStatus();      
+
+          }, 1000);
+
+        });
+
+       
+      });
+
+  }
+ 
+}
+
+
+
+function checkAgainStatus() {
+
+  console.log ("waiting for status... " + counter);
+
+  ScriptReadStatus(function (status) {
+
+    try {
+
+      console.log ("status received!");
+
+      console.log (JSON.stringify(status));
+
+      global_status = status;
+
+      checking = false;
+
+      otheraction = true;
+
+      setUIInitializationNeeded();
+
+      if (status.ospackagestatus == "not installed") {
+
+        if (status.internetconnection != "connected") {
+
+          setUIInternetconnectionNeeded();
+
+          otheraction = true;
+
+        } 
+      }
+
+      if (status.npmpackagestatus == "not installed") {
+
+        if (status.internetconnection != "connected") {
+
+          setUIInternetconnectionNeeded();
+
+          otheraction = true;
+
+        } 
+
+      }
+
+      if (status.ospackageinstallationstatus == "installing") {
+
+        setUIInstallationOngoing();
+
+        otheraction = true;
+
+      }
+
+      if (status.npmpackageinstallationstatus == "installing") {
+
+        setUIInstallationOngoing();
+
+        otheraction = true;
+
+      }
+
+      if (status.ospackagestatus == "installed" && status.npmpackagestatus == "installed" && status.dadinstallationstatus == "installed") {
+
+        setUISuccess();        
+
+      }
+
+    } catch (e) {
+
+
+    }
+
+  }, function() {
+
+    // no status yet
+
+    if (counter < waitseconds) {
+
+      counter = counter + 1;
+
+      is = setTimeout (function cs() {
+
+          checkAgainStatus();      
+
+        }, 1000);
+
+    }
+
+  })
+
+  if (counter >= waitseconds) {
+
+    console.log ("sorry, no status available.");
+
+    setUIFailed();
+
+  }
+
+}
+
+
+function setUISuccess() {
+
+  document.getElementById("install-status").innerHTML = "<img src='/assets/greencheck.png' >&nbsp;Dial-a-device is installed on this BeagleBone.&nbsp;"+ "<button onclick='install()' class='btn btn-success'>Reinstall</button>";
+
+  document.getElementById("submitbutton").class = "btn btn-success";
+
+}
+
+function setUIFailed() {
+
+  document.getElementById("install-status").innerHTML = "<img src='/assets/error.png' >&nbsp;Check failed.&nbsp;"+ "<button onclick='checkStatus()' class='btn btn-warning'>Check again</button>";
+
+  document.getElementById("submitbutton").class = "btn btn-success disabled";
+
+}
+
+function setUIStatusCheckOngoing() {
+
+  document.getElementById("install-status").innerHTML = "<img src='/assets/ajax-violet.gif' >&nbsp;Checking installation status, please wait...&nbsp;";
+
+  document.getElementById("submitbutton").class = "btn btn-success disabled";
+
+}
+
+function setUIInstallationOngoing() {
+
+  document.getElementById("install-status").innerHTML = "<img src='/assets/ajax-violet.gif' >&nbsp;The installation is ongoing and will take 5-10 min depending upon network connection.&nbsp;<button onclick='cancel()' class='btn btn-warning'>Cancel</button>";
+
+  document.getElementById("submitbutton").class = "btn btn-success disabled";
+
+}
+
+function setUIInitializationNeeded() {
+
+  document.getElementById("install-status").innerHTML = "<img src='/assets/error.png' >&nbsp;Dial-a-device needs to be installed on this BeagleBone.&nbsp;"+ "<button onclick='install()' class='btn btn-success'>Install</button>";
+
+  document.getElementById("submitbutton").class = "btn btn-success disabled";
+
+}
+
+function setUIInternetconnectionNeeded() {
+
+  document.getElementById("install-status").innerHTML = "<img src='/assets/error.png' >&nbsp;The BeagleBone needs to be connected to the internet via ethernet cable to install required packages.&nbsp;"+ "<button onclick='checkStatus()' class='btn btn-warning'>Check again</button>";
+
+  document.getElementById("submitbutton").class = "btn btn-success disabled";
+
+}
+
+
+
+function install() {
+
+  var status = global_status;
+
+
+  if (status.ospackageinstallationstatus === "not installing" && status.ospackagestatus === "not installed") {
+
+    // install ospackages
+
+    console.log ("install os packages");
+
+    var b = require('bonescript');
+
+    file = '/var/lib/cloud9/autorun/installospackages.js';
+    b.writeTextFile(file, "var exec = require ('child_process').exec; exec ('opkg update && opkg install python-compiler && opkg install python-misc && opkg install python-multiprocessing &> /var/lib/cloud9/installospackages.log && rm /var/lib/cloud9/autorun/installospackages.js', function(error, stdout, stderr) {console.log (stdout)});", function(x) {
+
+      otheraction = true;
+      setUIInstallationOngoing();
+
+
+    });
+
+  } else if (status.ospackageinstallationstatus == "not installing" && status.ospackagestatus == "installed" && status.npmpackageinstallationstatus == "not installing" && status.npmpackagestatus == "not installed") {
+
+    // install npmpackages
+
+    console.log ("install npm packages");
+
+    var b = require('bonescript');
+
+    file = '/var/lib/cloud9/autorun/installnpmpackages.js';
+    b.writeTextFile(file, "var exec = require ('child_process').exec; exec ('npm update && npm install dial-a-device-node &> /var/lib/cloud9/installnpmpackages.log && rm /var/lib/cloud9/autorun/installnpmpackages.js', function(error, stdout, stderr) {console.log (stdout)});", function(x) {
+
+      otheraction = true;
+      setUIInstallationOngoing();
+
+
+    });
+
+  } else if (status.ospackageinstallationstatus == "not installing" && status.ospackagestatus == "installed" && status.npmpackageinstallationstatus == "not installing" && status.npmpackagestatus == "installed") {
+
+    
+    console.log ("install dial-a-device");
+
+    bbSetServer();
+
+    // install dad in autorun
+
+    var b = require('bonescript');
+
+    file = '/var/lib/cloud9/autorun/dial-a-device-node.js';
+    b.writeTextFile(file, "var b = require('bonescript'); var dialadevicenode = require ('dial-a-device-node'); b.readTextFile('/var/lib/cloud9/server.txt', function(x) { if ((x.data != null) && (x.data.length != 0)) { dialadevicenode.run_beaglebone(x.data); } });", function(x) {
+
+      otheraction = false;
+      setUIInstallationOngoing();
+
+
+    });
+
+  
+  }  
+
+  checkStatus();
+
+}
+
+// old routines
+
+
+function checkStatusOld(){
+
+ 
+ var b = require('bonescript');
+ b.readTextFile('/var/lib/cloud9/autorun/forever.js', printStatus);
+
+function printStatus(x)
+ {
+ var c = require('bonescript');
+ c.readTextFile('/var/lib/cloud9/README.md', printStatus);
+
+function printStatus(y)
+ {
+var d = require('bonescript');
+ d.readTextFile('/var/lib/cloud9/autorun/Step-2.js', printStatus);
+
+function printStatus(z)
+ {
+  var isInstalled = false;
+  var isInstallationOngoing = false;
+  var isinternetconnection =false;
+  
+    if(x.data != null){  //if forever.js exists
+      if(x.data.length != 0)   //if forever.js is not empty
+      {
+        isInstalled = true;        
+      }
+  }
+  
+  if(z.data !=null){
+ isInstallationOngoing = true;
+}
+  if(y.data !=null){ 
+    isinternetconnection = true;
+  }
+
+
+ if (isInstallationOngoing) {
+
+    if(isinternetconnection){
+
+      if (isInstalled) { 
+        document.getElementById("install-status").innerHTML = '<img src="/assets/greencheck.png" >&nbsp;The connected device is initialized and installed properly.&nbsp;'+ "<button onclick='initialize()' class='btn btn-warning'>Re-initialize</button>";
+      } else{
+          document.getElementById("install-status").innerHTML = "<img src='/assets/ajax-violet.gif' >&nbsp;The installation is ongoing and will take 5-10 min depending upon network connection.&nbsp;<button onclick='initialize()' class='btn btn-warning'>Re-Initialize</button>";
+      }
+
+    } else{
+
+      document.getElementById("install-status").innerHTML ="<img src='/assets/error.png' >&nbsp;Error in Installation:Please check the internet connection and Re-Initialize.&nbsp;" + "<button onclick='initialize()' class='btn btn-danger'>Re-Initialize</button>";
+    }
+ 
+
+  } else {
+
+        document.getElementById("install-status").innerHTML = "<img src='/assets/ajax-violet.gif' >&nbsp;The device connected needs to be initialized.&nbsp;"+ "<button onclick='initialize()' class='btn btn-success'>Initialize</button>";
+      }
+
+
+
+}
+
+}
+}
+
+        }
+
+
+
+function initializeOld(){
 
   // dellogfiles();
 
@@ -119,14 +661,6 @@ function initialize(){
           } }}}}}}
 
 
-      function stepWriteServer() {
-
-        var bs = require('bonescript');
-        file = '/var/lib/cloud9/server.txt';
-       bs.writeTextFile(file, document.location.host);
-
-      }
-
       function step1(){
       var a = require('bonescript');
         file = '/var/lib/cloud9/autorun/Step-1.js';
@@ -227,75 +761,4 @@ function dellogfiles(){
   
 
 };
-
-
-
-
-function checkStatus() {
-
-            
- 
- var b = require('bonescript');
- b.readTextFile('/var/lib/cloud9/autorun/forever.js', printStatus);
-
-function printStatus(x)
- {
- var c = require('bonescript');
- c.readTextFile('/var/lib/cloud9/README.md', printStatus);
-
-function printStatus(y)
- {
-var d = require('bonescript');
- d.readTextFile('/var/lib/cloud9/autorun/Step-2.js', printStatus);
-
-function printStatus(z)
- {
-  var isInstalled = false;
-  var isInstallationOngoing = false;
-  var isinternetconnection =false;
-  
-    if(x.data != null){  //if forever.js exists
-      if(x.data.length != 0)   //if forever.js is not empty
-      {
-        isInstalled = true;        
-      }
-  }
-  
-  if(z.data !=null){
- isInstallationOngoing = true;
-}
-  if(y.data !=null){ 
-    isinternetconnection = true;
-  }
-
-
- if (isInstallationOngoing) {
-
-    if(isinternetconnection){
-
-      if (isInstalled) { 
-        document.getElementById("install-status").innerHTML = '<img src="/assets/greencheck.png" >&nbsp;The connected device is initialized and installed properly.&nbsp;'+ "<button onclick='initialize()' class='btn btn-warning'>Re-initialize</button>";
-      } else{
-          document.getElementById("install-status").innerHTML = "<img src='/assets/ajax-violet.gif' >&nbsp;The installation is ongoing and will take 5-10 min depending upon network connection.&nbsp;<button onclick='initialize()' class='btn btn-warning'>Re-Initialize</button>";
-      }
-
-    } else{
-
-      document.getElementById("install-status").innerHTML ="<img src='/assets/error.png' >&nbsp;Error in Installation:Please check the internet connection and Re-Initialize.&nbsp;" + "<button onclick='initialize()' class='btn btn-danger'>Re-Initialize</button>";
-    }
- 
-
-  } else {
-
-        document.getElementById("install-status").innerHTML = "<img src='/assets/ajax-violet.gif' >&nbsp;The device connected needs to be initialized.&nbsp;"+ "<button onclick='initialize()' class='btn btn-success'>Initialize</button>";
-      }
-
-
-
-}
-
-}
-}
-
-        }
 
