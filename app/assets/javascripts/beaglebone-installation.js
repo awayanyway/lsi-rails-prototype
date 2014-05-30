0b3a4165be0d7dea0a4f2e3fdf38f4aad57667af
Cominch <dominic@luetjohann.com>
2014-05-30 14:12:40 +0200

    bb installation procedure fix

--- app/assets/javascripts/beaglebone-installation.js
+++ app/assets/javascripts/beaglebone-installation.js
@@ -806,13 +806,13 @@ function install(what) {
       file = '/var/lib/cloud9/autorun/dial-a-device-node.js';
       b.writeTextFile(file, "forever = require ('forever-monitor');"
 
-        +"var child = new (forever.Monitor)('start.js', {"
-        +"  silent: false,"
-        +"  sourceDir: '/var/lib/cloud9',"
-        *"  killTree: true,"
-        *"  outFile: '/var/lib/cloud9/dial-a-device-node.log',"
-        +"  options: []"
-        +"});"
+        +"var child = new (forever.Monitor)('start.js', { "
+        +"  silent: false, "
+        +"  sourceDir: '/var/lib/cloud9', "
+        *"  killTree: true, "
+        *"  outFile: '/var/lib/cloud9/dial-a-device-node.log', "
+        +"  options: [] "
+        +" }); "
         +"child.start();"
 
         +"", function(x) {
