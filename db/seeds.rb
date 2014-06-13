13493e822a8c19e6da38e69469c746509a0e9da9
Cominch <dominic@luetjohann.com>
2014-06-13 16:47:11 +0200

    adjust device parameters for knf pump

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 # Devicetype.delete_all
 if (!Devicetype.exists?(:deviceclass_id => 1)) then
-	Devicetype.create :deviceclass_id => 1, :showcase => true, :name => "knf_sc920", :displayname => "KNF SC920", :porttype => "serial", :portname => "/dev/ttyACM0", :portbaud => "115200", :portlinebreak => "0A", :portdatabits => 8, :portparity => "none", :portstopbits => 1, :portprefix => "", :portsuffix => "0A"
+	Devicetype.create :deviceclass_id => 1, :showcase => true, :name => "knf_sc920", :displayname => "KNF SC920", :porttype => "serial", :portname => "/dev/ttyACM0", :portbaud => "115200", :portlinebreak => "0D", :portdatabits => 8, :portparity => "none", :portstopbits => 1, :portprefix => "", :portsuffix => "0D"
 else
 	Devicetype.where(["deviceclass_id = ?", 1]).first.update_attributes(:showcase => true, :name => "knf_sc920", :displayname => "KNF SC920", :porttype => "serial", :portname => "/dev/ttyACM0", :portbaud => "115200", :portlinebreak => "0A", :portdatabits => 8, :portparity => "none", :portstopbits => 1, :portprefix => "", :portsuffix => "0A")
 end
