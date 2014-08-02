898b302b8a1c33e2f0863c3cb645e8c8144db534
Cominch <dominic@luetjohann.com>
2014-08-02 12:34:14 +0200

    js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require_directory .
-//= require bootstrap
 //= require bootstrap-sprockets
 //= require cocoon
 //= require bootstrap-wysiwyg
